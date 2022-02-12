aad65b397582a654785094934f72f7576370775f
yurileeeee <yuriuniv@gmail.com>
2022-02-12 18:37:07 +0900

    Feat: getServerSideProps 사용해보기

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,18 @@
+import React, { useState } from "react";
 import Link from "next/link";
-import { useState } from "react";
-import { useRouter } from "next/router";
 
 const App = () => {
-    const [name, setName] = useState("");
-    const router = useRouter();
+    const [username, setUsername] = useState("");
     return (
         <div>
-            <button type="button" onClick={() => router.push("/tomato")}>
-                tomato로 가기
-            </button>
-            <p>이름</p>
-            <input value={name} onChange={(e) => setName(e.target.value)} style={{ marginRight: "12px" }} />
-            <button type ="button" onClick={() => router.push(`/vegetable/${name}`)}>
-                {name}으로 가기
-            </button>
+            <label>
+                username
+                <input value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <p>{username} 깃허브 검색하기</p>
+            <Link href={`/users/${username}`}>
+                <a>검색하기</a>
+            </Link>
         </div>
     );
 };
