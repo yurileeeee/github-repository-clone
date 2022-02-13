595de6ea02ac9c79a715ea76f1dd453b0e8a37ae
yurileeeee <yuriuniv@gmail.com>
2022-02-13 16:05:23 +0900

    Feat: _error를 사용해 에러페이지 만들기

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import Header from "../components/Header";
 
 const MyApp = ({ Component, pageProps}) => {
+  // * 일부러 에러를 발생시키는 코드
+  // useEffect(() => {
+  //   throw Error();
+  // }, []);
+  
   return (
     <>
       <Header />
