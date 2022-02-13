78996f984b6a299ed77857d02fc184e6c7a2c2ee
yurileeeee <yuriuniv@gmail.com>
2022-02-13 15:51:28 +0900

    Feat: _app 파일을 이용해 공통 헤더 만들기

---
+++ pages/_app.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+
+const MyApp = ({ Component, pageProps}) => {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body {
+          margin: 0;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default MyApp;
