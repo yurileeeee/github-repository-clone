ab61e1d115e5973dd0b26407d95dffa7271e20ae
yurileeeee <yuriuniv@gmail.com>
2022-02-13 13:31:54 +0900

    Feat: 레파지토리 날짜 형식 출력

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,5 +1,6 @@
 import css from "styled-jsx/css";
 import fetch from "isomorphic-unfetch";
+import formatDistance from "date-fns/formatDistance";
 import Profile from "../../components/Profile";
 
 const style = css`
@@ -82,7 +83,9 @@ const name = ({ user, repos }) => {
             <p className="repository-description">{repo.description}</p>
             <p className="repository-language">
               {repo.language}
-              <span className="repository-updated-at"></span>
+              <span className="repository-updated-at">
+                {formatDistance(new Date(repo.updated_at), new Date(), {addSuffix: true,})}
+              </span>
             </p>
           </div>
         )))}
